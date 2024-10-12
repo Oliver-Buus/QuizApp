59083406caf431adfdbaf554685bec86ab1c2e2b
Oliver <oliverbuus@live.dk>
2024-10-12 11:13:30 +0200

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by dmu mac 24 on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizTests/QuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizTests.swift
+//  QuizTests
+//
+//  Created by dmu mac 24 on 12/10/2024.
+//
+
+import Testing
+@testable import Quiz
+
+struct QuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
