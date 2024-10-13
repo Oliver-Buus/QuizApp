e140243ff9062ce527bf6e91794b5a418a44b119
Oliver <oliverbuus@live.dk>
2024-10-13 14:31:01 +0200

    A bit functionality but still messy

---
+++ Quiz/Views/QuestionAmountView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionAmountView.swift
+//  Quiz
+//
+//  Created by dmu mac 24 on 13/10/2024.
+//
+
+import SwiftUI
+
+struct QuestionAmountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionAmountView()
+}
